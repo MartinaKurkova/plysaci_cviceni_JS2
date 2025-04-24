89025d48f9408957897466361ef10a76f87714f2
Martina Kurkova <martinanoha@seznam.cz>
2025-04-24 14:59:18 +0200

    hotovo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,17 +2,45 @@ import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
 
+const silvestr = {
+  name: "Silvestr",
+  image: "https://kodim.cz/cms/assets/czechitas/js2/lekce/sablony-komponenty/cv-komponenty/plysaci/elephant.jpg",
+  text: "Silvestr rád pozoruje dění za oknem a upřímně se usmívá na všechno kolem.",
+}
+
+const ctirad = {
+  name: "Ctirad",
+  image: "https://kodim.cz/cms/assets/czechitas/js2/lekce/sablony-komponenty/cv-komponenty/plysaci/mouse.jpg",
+  text:"Ctirad tráví svůj čas v blízkosti lednice a s očekáváním pozoruje její bílé dveře.",
+}
+
+const PlushySilvestr = () => {
+  return (
+    <div className="plushy">
+    <img className="plushy__image" src={silvestr.image} />
+    <h2 className="plushy__name">{silvestr.name} </h2>
+    <p className="plushy__text">{silvestr.text} </p>
+</div>
+  )
+}
+
+const PlushyCtirad = () => {
+  return (
+    <div className="plushy">
+    <img className="plushy__image" src={ctirad.image} />
+    <h2 className="plushy__name">{ctirad.name} </h2>
+    <p className="plushy__text">{ctirad.text} </p>
+</div>
+  )
+}
+
+
 document.querySelector('#root').innerHTML = render(
   <div className="container">
-    <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace</h1>
-    </header>
-    <main>
-      <p>Startovací šablona pro webovou aplikaci v JavaScriptu s JSX. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-kodim-app">create-kodim-app</a>.</p>
-    </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
+  <h1>Plyšáci</h1>
+    <div className="plushies">
+      <PlushySilvestr />
+      <PlushyCtirad />
+    </div>
   </div>
 );
